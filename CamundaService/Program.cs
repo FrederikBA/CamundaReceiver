dfa1c88be823a1ef93de45c8d9eb1d38901dc323
FrederikBA <frederikbandersen98@gmail.com>
2023-10-22 13:15:37 +0200

    refactor

--- CamundaService/Program.cs
+++ CamundaService/Program.cs
@@ -5,12 +5,11 @@ using CamundaService.Models.Dtos;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-var factory = new ConnectionFactory { HostName = "localhost" }; 
-var connection = factory.CreateConnection(); 
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
 using var channel = connection.CreateChannel();
 
 channel.QueueDeclare("camunda");
-
 Console.WriteLine(" [*] Waiting for messages.");
 
 var consumer = new EventingBasicConsumer(channel);
@@ -19,9 +18,10 @@ consumer.Received += (model, eventArgs) =>
     var body = eventArgs.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
     var dto = JsonSerializer.Deserialize<CompleteTaskDto>(message);
+    
+    //Complete Camunda Task
     var camundaTask = new CamundaTask();
     camundaTask.CompleteTask(dto);
-    Console.WriteLine(message);
 };
 
 channel.BasicConsume(queue: "camunda", autoAck: true, consumer: consumer);
